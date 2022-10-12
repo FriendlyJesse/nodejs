1c527e7d94f8327f564e8eabb70a2b02394ab934
Jesse <jessexinyu@foxmail.com>
2022-10-12 17:30:26 +0800

    学习结束

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-const express = require('express')
-const formidable = require('express-formidable')
+// const express = require('express')
+// const formidable = require('express-formidable')
 
-const app = express()
-const router = require('./src/router/index')
+// const app = express()
+// const router = require('./src/router/index')
 
 
-function nw (req, res, next) {
-	console.log('这是一个简单的中间件')
-	next()
-}
+// function nw (req, res, next) {
+// 	console.log('这是一个简单的中间件')
+// 	next()
+// }
 
-// app.use(formidable())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('./static'))
-app.use('/api', router) 
-app.use(nw)
+// // app.use(formidable())
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.static('./static'))
+// app.use('/api', router) 
+// app.use(nw)
 
-app.get('/test', (req, res) => {
-  console.log(req.query)
-  res.send({
-    name: 'test',
-    age: 20,
-    gender: '男'
-  })
-})
-app.post('/test', (req, res) => {
-  console.log(req)
-  res.send({
-    name: 'test',
-    age: 20,
-    gender: '女'
-  })
-})
+// app.get('/test', (req, res) => {
+//   console.log(req.query)
+//   res.send({
+//     name: 'test',
+//     age: 20,
+//     gender: '男'
+//   })
+// })
+// app.post('/test', (req, res) => {
+//   console.log(req)
+//   res.send({
+//     name: 'test',
+//     age: 20,
+//     gender: '女'
+//   })
+// })
 
-app.listen(8080, () => {
-  console.log('server running at http://localhost:8080')
-})
+// app.listen(8080, () => {
+//   console.log('server running at http://localhost:8080')
+// })
+
+require('./src/db')
--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,44 @@
 const mysql = require('mysql2')
 
 const db = mysql.createPool({
-  host: 'localhost',
+  host: '192.168.0.141',
   port: '3307',
   user: 'root',
   password: '123456',
   database: 'test'
 })
 
-db.query('SELECT 1', (err, results) => {
-  console.log(err, results)
-})
+// db.query('SELECT * FROM users', (err, results) => {
+//   if (err) return console.log(err.message)
+//   console.log(results)
+// })
+
+// const user = {
+//   username: 'jesse3',
+//   password: 'jesse123'
+// }
+// // 直接将 ？ 表示按顺序插入的占位符
+// const sqlStr = 'INSERT INTO users SET ?'
+// // 将数据对象当作占位符的值
+// db.query(sqlStr, user, (err, results) => {
+//   if (err) return console.log(err.message)
+//   console.log(results)
+// })
+
+// const user = {
+//   id: 13,
+//   username: 'bbb',
+//   password: '000'
+// }
+// const sqlStr = 'UPDATE users SET ? WHERE id=?'
+// db.query(sqlStr, [user, user.id], (err, results) => {
+//   if (err) return console.log(err.message)
+//   if (results.affectedRows === 1) console.log('更新数据成功！')
+// })
+
+// const sqlStr = 'DELETE FROM users WHERE id=?'
+// const field = 14
+// db.query(sqlStr, field,(err, results) => {
+//   if (err) return console.log(err.message)
+//   if (results.affectedRows === 1) console.log('删除数据成功！')
+// })
