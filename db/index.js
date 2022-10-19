bd5fca6b85af81cb24fd6bf62fd51d89cfa88edc
jesse <jessexinyu@foxmail.com>
2022-10-19 09:53:13 +0800

    update

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 
 const db = mysql.createPool({
-  host: '192.168.0.141',
-  port: '3307',
-  user: 'root',
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'big_matter'
+  database: process.env.DB_DATABASE
 })
 
 module.exports = db
